af1a5df2e2e396ed05007cf5c1eddadc20192ea7
Mark Bell <me@markashleybell.com>
2012-02-27 08:04:39 +0000

    Tidying page API, update test harness

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -4,33 +4,72 @@ class Api_Controller extends Base_Controller {
 
 	public $restful = true;
 
-	public function get_pages()
+	// GET: GET /pages/1 or GET /pages/list
+	public function get_pages($id)
 	{
-		$pages = Page::where('deleted', '!=', true)->get();
+		// If the id is 'list', show all pages (in the future, this should just be '/pages')
+		if($id == 'list')
+		{
+			// TODO: This is not great, but Laravel doesn't seem to support 
+			// optional parameters for GET actions at the moment
 
-		$output = array();
+			$pages = Page::where('deleted', '!=', true)->get();
 
-		foreach ($pages as $page) {
-			$output[] = $page->attributes;
+			$output = array();
+
+			foreach ($pages as $page) {
+				$output[] = $page->attributes;
+			}
+
+			return Response::make(json_encode($output), 200, array('Content-Type' => 'application/json'));
 		}
 
-		return Response::make(json_encode($output), 200, array('Content-Type' => 'application/json'));
+		$fields = Input::get('fields');
+
+		// If there's a list of fields in the querystring
+		if($fields != null)
+		{
+			try
+			{
+				// Attempt to retrieve only the requested fields for this record
+				$page = Page::where('deleted', '!=', true)->where('id', '=', $id)->get(explode(',', $fields));
+
+				// If null, return 404
+				if($page == null)
+					return JSONUtils::createResponse(404, 'Not Found', 'No item exists with the requested id');
+
+				// In this case $page is actually an array because we used get() rather than first(), 
+				// so just return the attributes of the first element
+				return Response::make(json_encode($page[0]->attributes), 200, array('Content-Type' => 'application/json'));
+			}
+			catch(Exception $e)
+			{
+				// In most cases, this will be thrown if the client gets a field name wrong
+				return JSONUtils::createResponse(400, 'Bad Request', 'Property list contained one or more invalid property names');	
+			}
+		}
+		else // Client wants ALL data fields
+		{
+			$page = Page::where('deleted', '!=', true)->where('id', '=', $id)->first();
+
+			if($page == null)
+				return JSONUtils::createResponse(404, 'Not Found', 'No item exists with the requested id');
+
+			return Response::make(json_encode($page->attributes), 200, array('Content-Type' => 'application/json'));
+		}
 	}
 
-	public function get_page($id)
+	// CREATE: POST /pages
+	public function post_pages($id)
 	{
-		$page = Page::where('deleted', '!=', true)->where('id', '=', $id)->first();
+		// Prevent POST requests to a url with an id segment
+		if($id != null)
+			return JSONUtils::createResponse(400, 'Bad Request', 'POST requests to an item/id url are not supported');
 
-		if($page == null)
-			return JSONUtils::createResponse(404, 'Not Found', 'No item exists with the requested id');
-
-		return Response::make(json_encode($page->attributes), 200, array('Content-Type' => 'application/json'));
-	}
-
-	public function post_page()
-	{
+		// Create a new page from POST data
 		$page = new Page;
 
+		// TODO: need some input validation here! Does Laravel help with this already?
 		$page->title = Input::get('title');
 		$page->displayorder = Input::get('displayorder');
 
@@ -39,35 +78,46 @@ class Api_Controller extends Base_Controller {
 
 		$page->save();
 
+		// Return all the details of the new page as a JSON response
 		return Response::make(json_encode($page->attributes), 201, array('Content-Type' => 'application/json'));
 	}
 
-	public function put_page($id)
+	// UPDATE: PUT /pages/1
+	public function put_pages($id)
 	{
+		// Get the existing page data
 		$page = Page::where('deleted', '!=', true)->where('id', '=', $id)->first();
 
+		// If the page doesn't exist, return a helpful message
 		if($page == null)
 			return JSONUtils::createResponse(404, 'Not Found', 'No item exists with the requested id');
 
+		// Update the data from PUT data fields
 		$page->title = Input::get('title');
 		$page->displayorder = Input::get('displayorder');
 
 		$page->save();
 
+		// Return all the details of the updated page as a JSON response
 		return Response::make(json_encode($page->attributes), 200, array('Content-Type' => 'application/json'));
 	}
 
-	public function delete_page($id)
+	// DELETE: DELETE /pages/1
+	public function delete_pages($id)
 	{
+		// Get the existing page data
 		$page = Page::where('deleted', '!=', true)->where('id', '=', $id)->first();
 
+		// If the page doesn't exist, return a helpful message
 		if($page == null)
 			return JSONUtils::createResponse(404, 'Not Found', 'No item exists with the requested id');
 
+		// 'Soft delete' to allow for undo functionality
 		$page->deleted = true;
 
 		$page->save();
 
+		// Return the details of the deleted page as a JSON response
 		return Response::make(json_encode($page->attributes), 200, array('Content-Type' => 'application/json'));
 	}
 }
---
+++ schema.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 27, 2012 at 07:31 AM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `gtd-pad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE IF NOT EXISTS `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `displayorder` int(11) NOT NULL,
+  `page_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `listitems`
+--
+
+CREATE TABLE IF NOT EXISTS `listitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `body` text NOT NULL,
+  `displayorder` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pages`
+--
+
+CREATE TABLE IF NOT EXISTS `pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `displayorder` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
