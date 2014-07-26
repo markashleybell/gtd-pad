3eb7e30183321ab7c304a23a864b853f87bd5533
Mark Bell <me@markashleybell.com>
2014-07-26 11:49:40 +0100

    Implement test harness

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import json
 import psycopg2
 import psycopg2.extras
 import psycopg2.extensions
@@ -6,8 +7,9 @@ from psycopg2.pool import ThreadedConnectionPool
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
 
-from flask import Flask, render_template, request, send_from_directory, \
+from flask import Flask, Response, render_template, request, send_from_directory, \
      redirect, url_for, jsonify, flash
+from flask.json import JSONEncoder
 from flask.ext.login import LoginManager, current_user, login_required, \
      login_user, logout_user, UserMixin, confirm_login, fresh_login_required
 from contextlib import contextmanager
@@ -48,6 +50,11 @@ class User(UserMixin):
         self.id = id
 
 
+class ApiResponse(Response):
+    def __init__(self, payload=None, status_code=200, message='OK'):
+        Response.__init__(self, json.dumps({ 'status': status_code, 'message': message, 'payload': payload }, cls=JSONEncoder, indent=4, separators=(',', ': ')), status=status_code, mimetype='application/json')
+
+
 @login_manager.user_loader
 def load_user(userid):
     """Callback to load user from db, called by Flask-Login"""
@@ -70,6 +77,7 @@ connection_pool = ThreadedConnectionPool(1, 20, app.config['CONNECTION_STRING'])
 @login_required
 def page(id=1):
     pagedata = None
+
     with get_connection() as conn:
         cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
         cur.execute('SELECT title FROM pages WHERE id = %s', [id])
@@ -78,9 +86,31 @@ def page(id=1):
     return render_template('page.html', pagedata=pagedata)
 
 
-@app.route('/login', methods=['GET'])
-def login():
-    return render_template('login.html', next=request.args['next'])
+@app.route('/api/v1/pages', methods=['GET'])
+def get_pages():
+    pages = None
+
+    with get_connection() as conn:
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur.execute('SELECT * FROM pages WHERE deleted = False', [])
+        pages = cur.fetchall()
+
+    return ApiResponse(pages)
+
+
+@app.route('/api/v1/pages/<int:id>', methods=['GET'])
+def get_page(id):
+    page = None
+
+    with get_connection() as conn:
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur.execute('SELECT * FROM pages WHERE id = %s AND deleted = False', [id])
+        page = cur.fetchone()
+
+    if page is None:
+        return ApiResponse(message='Not Found', status_code=404)
+
+    return ApiResponse(page)
 
 
 @app.route('/login', methods=['POST'])
@@ -108,9 +138,22 @@ def logout():
     return redirect('/')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    if 'application/json' in request.headers['Accept']:
+        return ApiResponse(message='Not Found', status_code=404)
+    return render_template('404.html'), 404
+
+
 @app.route('/robots.txt')
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
+
+
+@app.route('/api-test-harness', methods=['GET'])
+@login_required
+def api_test_harness():
+    return render_template('api-test-harness.html')
 
 
 if __name__ == '__main__':
